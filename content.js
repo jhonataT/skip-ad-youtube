4884a96714234a568e318134952912057fa6782e
Jhonata Tenório <51134324+JhN-Te@users.noreply.github.com>
2020-11-23 23:26:31 -0300

    Update content.js

--- content.js
+++ content.js
@@ -1,23 +1,25 @@
 const interval = setInterval( () => {
-    const beforeAd = document.querySelector(".ytp-ad-text.ytp-ad-preview-text");
-    const boxAd = document.querySelector(".ytp-ad-overlay-close-button");
-    if(boxAd){
-        console.log("FECHANDO!");
+    const beforeSkipAd = document.querySelector(".ytp-ad-text.ytp-ad-preview-text");
+    const closeBoxAd = document.querySelector(".ytp-ad-overlay-close-button");
+    if(beforeSkipAd){
+        console.log(`
+        ╔═╗ ╔╦╗
+        ╠═╣  ║║
+        ╩ ╩o═╩╝
+        `);
+        const showInfo = document.querySelector(".style-scope.ytd-masthead");
+        const skipAd = document.querySelector(".ytp-ad-text.ytp-ad-skip-button-text");
+        if(skipAd)
+            document.querySelector(".ytp-ad-text.ytp-ad-skip-button-text").click();
+    }
+    if(closeBoxAd){
+        console.log(`
+        ╔═╗ ╔╦╗
+        ╠═╣  ║║
+        ╩ ╩o═╩╝
+        `);
+        const showInfo = document.querySelector(".style-scope.ytd-masthead");
+        const button = document.createElement("button");
         document.querySelector(".ytp-ad-overlay-close-button").click(); 
     }
-    
-    if(beforeAd){
-        console.log("OLHA O AD CHEGANDO!");
-        const afterAd = document.querySelector(".ytp-ad-text.ytp-ad-skip-button-text");
-
-        if(afterAd){
-            console.log("SKIPANDO");
-            document.querySelector(".ytp-ad-text.ytp-ad-skip-button-text").click();
-        }
-    } 
-    else console.log("No aguardo")
-    
 }, 1000);
-
-
-
